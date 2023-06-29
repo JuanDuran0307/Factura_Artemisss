85386203c7727673c2c856a59bd94285e52d1ad3
JuanDuran0307 <juanalexanderquinchoa@gmail.com>
2023-06-29 09:08:57 -0500

    feat: ..
    .

--- Backend/src/controllers/categorias.controller.js
+++ Backend/src/controllers/categorias.controller.js
@@ -12,9 +12,59 @@ const getCategorias = async(req, res) =>{
     }
 
 
+const addCategories = async(req,res)=>{
+    try {
+        
+        const {CategoriaNombre,Descripcion,Imagen} = req.body;
+        const category = {CategoriaNombre,Descripcion,Imagen};
+        const connection = await getConection();
+        const result = await connection.query("INSERT INTO categorias SET ?",category);
+        console.log(result);
+        res.json(result);
+        
+    } catch (error) {
+        res.status(500);
+        res.send(error.message)
+            
+    }
+}
 
+const getCategoria = async(req,res)=>{
+    try {
+        console.log(req.params);
+        const {id} = req.params;
+        const connection = await getConection();
+        const result = await connection.query("SELECT categoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias WHERE categoriaID = ?",id);
+        console.log(result);
+        res.json(result);
+        
+    } catch (error) {
+        res.status(500);
+        res.send(error.message)
+        
+    }
+}
+
+}
+const deleteCategoria = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const connection = await getConection();
+        const result = await connection.query("DELETE FROM categorias WHERE categoriaID = ?",id);
+        console.log(result);
+        res.json(result);
+        
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+        
+    }
 }
 
 export const methodsHTTP = {
-    getCategorias
+    getCategorias,
+    addCategories,
+    getCategoria,
+    deleteCategoria,
+    updateCategories
 }
--- Backend/src/routes/categorias.routes.js
+++ Backend/src/routes/categorias.routes.js
@@ -5,6 +5,10 @@ import { methodsHTTP as categoriaController } from "../controllers/categorias.co
 const router = Router();
 
 router.get("/", categoriaController.getCategorias); 
+router.post("/", categoriaController.addCategories); 
+router.get("/:id", categoriaController.getCategoria); 
+router.delete("/:id", categoriaController.deleteCategoria); 
+router.put("/:id", categoriaController.updateCategoria); 
 
 export default router;
 
